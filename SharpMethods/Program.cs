c268c7091565fbae6ee23b60abefbab9dba0e1eb
SolomonDa <solomonda@stlghana.com>
2022-11-17 17:46:48 +0000

    Add project files.

---
+++ SharpMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SharpMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstVal = 23;
+            int secondVal = 23432;
+            AddThem(firstVal, secondVal);
+
+            int res = getSub(secondVal, firstVal);
+            Console.WriteLine($"DIVISION --> {res}");
+        }
+
+        static int AddThem(int a, int b)
+        {
+            Console.WriteLine($"SUMMATION VALUE IS : {(a + b)}");
+            return a + b;
+        }
+
+        static int getSub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
